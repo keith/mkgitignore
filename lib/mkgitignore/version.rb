959da5ea4a55e692b5cf19764a9e90c3253d004b
Keith Smiley <keithbsmiley@gmail.com>
2012-12-17 14:20:48 -0500

    Corrected website for help

--- lib/mkgitignore/version.rb
+++ lib/mkgitignore/version.rb
@@ -1,5 +1,6 @@
 module Mkgitignore
   AUTHOR = "Keith Smiley"
+  WEBSITE = "https://github.com/Keithbsmiley/mkgitignore"
   EMAIL = "keithbsmiley@gmail.com"
   VERSION = "0.1.0"
   GITIGNORE_URL = "https://api.github.com/repos/github/gitignore/contents/"
