c4bc70b2c5642cf5341f981d280a168ebaee613e
Keith Smiley <keithbsmiley@gmail.com>
2013-04-15 20:16:13 -0400

    Bump version number

--- lib/mkgitignore/version.rb
+++ lib/mkgitignore/version.rb
@@ -1,9 +1,9 @@
 module Mkgitignore
-  AUTHOR = "Keith Smiley"
-  WEBSITE = "https://github.com/Keithbsmiley/mkgitignore"
-  EMAIL = "keithbsmiley@gmail.com"
-  VERSION = "0.1.2"
-  GITIGNORE_URL = "https://api.github.com/repos/github/gitignore/contents/"
-  GITIGNORE_FILE_NAME = ".gitignore"
-  BACKUP_FILE_NAME = "backup.gitignore"
+  AUTHOR = 'Keith Smiley'
+  WEBSITE = 'https://github.com/Keithbsmiley/mkgitignore'
+  EMAIL = 'keithbsmiley@gmail.com'
+  VERSION = '0.1.3'
+  GITIGNORE_URL = 'https://api.github.com/repos/github/gitignore/contents/'
+  GITIGNORE_FILE_NAME = '.gitignore'
+  BACKUP_FILE_NAME = 'backup.gitignore'
 end
